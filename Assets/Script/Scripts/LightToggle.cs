173b011f21e7b8b76e53c2dbc6903fa798943d0e
Katha Shah <7.katha@gmail.com>
2020-04-04 14:57:31 -0700

    Katha flashlight update (#17)
    Battery does not drain when the flashlight is turned off.

--- Assets/Script/Scripts/LightToggle.cs
+++ Assets/Script/Scripts/LightToggle.cs
@@ -7,51 +7,53 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 
-public class LightToggle : MonoBehaviour {
+public class LightToggle : MonoBehaviour
+{
 
-	[Header("Flashlight")]
-	private Light flashlight; // light component
-	
-	[Header("Random Number")]
-	float random1; // random number
-	float random2; // random number
-	
+    [Header("Flashlight")]
+    private Light flashlight; // light component
+
+    [Header("Random Number")]
+    float random1; // random number
+    float random2; // random number
+
     [Header("Flashlight Battery Settings")]
     public GameObject batterySlider;
     public float battery = 100;
     public float batteryMax = 100;
-    public float removeBatteryValue = 0.05f;
-    public float secondToRemoveBaterry = 5f;
+    public float removeBatteryValue = 3;
+    public float secondToRemoveBaterry = 6;
 
-	[Header("Torch On/Off SFX Settings")]
- 	public GameObject TorchOnOffSFXContainer; //the gameobject which contains the audiosource required
-	AudioSource TorchOnOffSFX; //on/off sfx for torch
+    [Header("Torch On/Off SFX Settings")]
+    public GameObject TorchOnOffSFXContainer; //the gameobject which contains the audiosource required
+    AudioSource TorchOnOffSFX; //on/off sfx for torch
 
     [Header("Battery Off SFX Settings")]
     public GameObject BatteryOutSFXContainer;
     AudioSource BatteryOffSFX;
 
-    //Update--Katha Shah--> flashlight flickering
+    //flashlight flickering
     public float maxFlickerSpeed = 1f;              //max light flicker speed
     public float minFlickerSpeed = 0.1f;            //min light flicker speed
     private bool isPlayedBatteryOut;
 
-    void Start(){
-		battery = batteryMax;
-		
-		flashlight = GetComponent<Light>();
-		
-		TorchOnOffSFX = TorchOnOffSFXContainer.GetComponent<AudioSource>(); // TorchOnOffSFX is the audiosource attached to TorchOnOffSFXContainer gameobject
+    void Start()
+    {
+        battery = batteryMax;
+
+        flashlight = GetComponent<Light>();
+
+        TorchOnOffSFX = TorchOnOffSFXContainer.GetComponent<AudioSource>(); // TorchOnOffSFX is the audiosource attached to TorchOnOffSFXContainer gameobject
         BatteryOffSFX = BatteryOutSFXContainer.GetComponent<AudioSource>();
         // set initial battery values
         batterySlider.GetComponent<Slider>().maxValue = batteryMax;
         batterySlider.GetComponent<Slider>().value = batteryMax;
-		
+
         // start consume flashlight battery
         StartCoroutine(RemoveBaterryCharge(removeBatteryValue, secondToRemoveBaterry));
 
         isPlayedBatteryOut = false;
-	}
+    }
 
     void Update()
     {
@@ -72,28 +74,29 @@ public class LightToggle : MonoBehaviour {
         yield return new WaitForSeconds(Random.Range(minFlickerSpeed, maxFlickerSpeed));
     }
 
-    void LateUpdate(){
-		// update baterry slider
+    void LateUpdate()
+    {
+        // update baterry slider
         batterySlider.GetComponent<Slider>().value = battery;
-		
-		random1 = Random.Range (0f, 100f); // get random number between 0 and 100
-		random2 = Random.Range (0f, 100f); // get random number between 0 and 100
-		
-		if (Input.GetKeyDown(KeyCode.F)) // if f is pressed
-		{
-			if (random1 >= random2)  // if random number is bigger than light range
-			{
-				flashlight.enabled = false; // disable the light component	
-				TorchOnOffSFX.Play(); //Play on/off SFX
-			}
-			else // or else
-			{
-				flashlight.enabled = true; // enable the light component
-				TorchOnOffSFX.Play(); //Play on/off SFX
-			}
-		}
-		
-		//battery part
+
+        random1 = Random.Range(0f, 100f); // get random number between 0 and 100
+        random2 = Random.Range(0f, 100f); // get random number between 0 and 100
+
+        if (Input.GetKeyDown(KeyCode.F)) // if f is pressed
+        {
+            if (random1 >= random2)  // if random number is bigger than light range
+            {
+                flashlight.enabled = false; // disable the light component	
+                TorchOnOffSFX.Play(); //Play on/off SFX
+            }
+            else // or else
+            {
+                flashlight.enabled = true; // enable the light component
+                TorchOnOffSFX.Play(); //Play on/off SFX
+            }
+        }
+
+        //battery part
         // if battery is low 50%
         if (battery / batteryMax * 100 <= 50)
         {
@@ -112,7 +115,7 @@ public class LightToggle : MonoBehaviour {
         if (battery / batteryMax * 100 <= 10)
         {
             // Debug.Log("You will be out of light.");
-            flashlight.intensity = 1.35f;           
+            flashlight.intensity = 1.35f;
         }
 
         // if battery out
@@ -120,7 +123,7 @@ public class LightToggle : MonoBehaviour {
         {
             battery = 0.00f;
             // Debug.Log("The flashlight battery is out and you are out of the light.");
-            
+
             flashlight.intensity = 0.0f;
             StopCoroutine("FlashLightModifier");
             flashlight.enabled = false;
@@ -133,20 +136,22 @@ public class LightToggle : MonoBehaviour {
 
         }
     }
-	
+
     public IEnumerator RemoveBaterryCharge(float value, float time)
     {
         while (true)
         {
             yield return new WaitForSeconds(time);
 
-            //Debug.Log("Removing baterry value: " + value);
-
-            if (battery > 0)
-                battery -= value;
-            else
-                // Debug.Log("The flashlight battery is out");
-                Debug.Log("EventLog " + System.DateTime.Now.ToString("HH:mm:ss") + " Flashlight is Out");
+            Debug.Log("Removing baterry value: " + value);
+            if (flashlight.enabled)
+            {
+                if (battery > 0)
+                    battery -= value;
+                else
+                    Debug.Log("The flashlight battery is out");
+                    
+            }
         }
     }
 }
